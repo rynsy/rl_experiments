fdcda2066b0f14799713e38c85b05548d5f6f3f1
Ryan Lindsey <ryan.lindsey@uky.edu>
2020-06-24 18:53:34 -0400

    Got a patch for the environment that helps me re-use generated boards. Need to write a call-back to allow me to re-generate levels when one is mastered

--- baselines_experiment/dqn.py
+++ baselines_experiment/dqn.py
@@ -5,17 +5,15 @@ from stable_baselines.deepq.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv 
 from stable_baselines import DQN
 
-"""
-    1. CNN policy (junk)
-    2. Mlp policy (better)
-    3. ???
-    4. double_q, prioritized replay
-
-"""
-
 env = gym.make('Sokoban-small-v0')
 
-model = DQN(MlpPolicy, env, verbose=1, tensorboard_log=".dqn/", double_q=True, prioritized_replay=True)
+model = DQN(MlpPolicy, env, 
+        verbose=1, 
+        tensorboard_log=".dqn/", 
+        double_q=True, 
+        prioritized_replay=True
+        )
+
 model.learn(total_timesteps=1_000_000)
 model.save(".dqn/dqn_boxoban")
 
