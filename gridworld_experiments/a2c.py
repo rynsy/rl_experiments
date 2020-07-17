35a1c5e36e39d64f4bdcf65d970c8d3dd1d19b53
Ryan Lindsey <ryan.lindsey@uky.edu>
2020-07-17 15:02:10 -0400

    Stopping training on reward threshold. Everything seems to work. Now to create a curriculum

--- gridworld_experiments/a2c.py
+++ gridworld_experiments/a2c.py
@@ -10,12 +10,13 @@ from stable_baselines.common.evaluation import evaluate_policy
 from stable_baselines.common.vec_env import SubprocVecEnv, DummyVecEnv
 from stable_baselines.common import set_global_seeds
 from stable_baselines import A2C
+from stable_baselines.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
 
-env_id = 'MiniGrid-DoorKey-16x16-v0'
-max_steps = 1_000_000
+env_id = 'MiniGrid-DoorKey-5x5-v0'
+max_steps = 10_000_000
 log_dir = ".a2c/"
 num_cpu = 8
-parallel = True
+parallel = True 
 
 def make_env(env_id, rank, seed=0):
     def _init():
@@ -31,29 +32,39 @@ if __name__ == "__main__":
     # Parallel environments
     if parallel:
         env = SubprocVecEnv([make_env(env_id, i) for i in range(num_cpu)])
+        reward_env = SubprocVecEnv([make_env(env_id, i) for i in range(1)])
     else:
         env = DummyVecEnv([make_env(env_id, i) for i in range(num_cpu)])
+        reward_env = DummyVecEnv([make_env(env_id, i) for i in range(1)])
+
+    eval_env = make_env(env_id, 0)()
 
     model = A2C(CnnPolicy, env, verbose=1, tensorboard_log=log_dir)
-    model.learn(total_timesteps=max_steps)
+
+    callback_on_best = StopTrainingOnRewardThreshold(reward_threshold=0.9, verbose=1)
+    eval_callback = EvalCallback(reward_env, callback_on_new_best=callback_on_best, eval_freq=10_000, verbose=1)
+
+    model.learn(total_timesteps=max_steps, callback=eval_callback)
     model.save(log_dir + "a2c_minigrid")
     del model # remove to demonstrate saving and loading
 
     model = A2C.load(log_dir + "a2c_minigrid")
-    env = make_env(env_id, 0)()
 
-    mean_reward, std_reward = evaluate_policy(model, env)
+    eval_env.reset()
+    mean_reward, std_reward = evaluate_policy(model, eval_env)
     print("Mean Reward: {}, std_dev: {}".format(mean_reward, std_reward))
+
+
     demo = input("Watch model? (q to quit)")
     if demo != "q":
         for _ in range(1000):
-            obs = env.reset()
+            obs = eval_env.reset()
             t = 0
             while t < 200:
                 action, _states = model.predict(obs)
-                obs, rewards, done, info = env.step(action)
+                obs, rewards, done, info = eval_env.step(action)
                 print("Action: {}\tTimestep: {}".format(action,t))
-                env.render(mode='human')
+                eval_env.render(mode='human')
                 t += 1
                 if done:
                     break
