1edab8efb4b137c887206dc1aa302c36e6d8a009
Ryan Lindsey <ryan.lindsey@uky.edu>
2021-10-27 15:36:59 -0400

    Checking in

--- baselines_experiment/a2c.py
+++ baselines_experiment/a2c.py
@@ -10,11 +10,11 @@ env = make_vec_env('Boxoban-Train-v0', n_envs=4)
 
 model = A2C(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=25000)
-model.save("a2c_sokoban")
+model.save("a2c_boxoban")
 
 del model # remove to demonstrate saving and loading
 
-model = A2C.load("a2c_sokoban")
+model = A2C.load("a2c_boxoban")
 
 obs = env.reset()
 while True:
